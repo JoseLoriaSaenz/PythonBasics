7cd7d17cec0d6438dd9674bdd10e0b71293dfecc
JoseLoriaSaenz <loriafjose@gmail.com>
2023-07-18 11:47:48 -0600

    First Steps

---
+++ 00_Hello.py
@@ -0,0 +1,39 @@
+import os
+os.system('cls')
+
+# Singleline Comments in python are specify with hashtag sign
+"""
+Multilines Comments in pytho are specify 
+using three double quotes
+"""
+'''
+Multiline Comnent in python
+using three single quotes
+
+'''
+print("\n")
+print('Output Console Text using print')
+print("Hello Python - double quoted string") 
+print('Hello Python - single quotes string')
+
+# Check object type
+print("\n")
+print('Check Python Types')
+print(type('Hola Python'))
+print(type(5))
+print(type(True))
+print(type(1.5))
+print(type(3 + 1j))
+print(type([1,2,3]))
+print(type({'name':'Jose'}))
+print(type({9.8, 3.17, 2.7}))
+print(type((3.14, 2.7, 9.8)))
+
+# Exceuting operations
+print("\n")
+print('Executing Calculations')
+print(3 * 2)
+print( 6 ** 3)
+print( 6 + 3)
+print( 6 - 3)
+print( 6 / 3)
---
+++ 01_Variables.py
@@ -0,0 +1,29 @@
+import os
+os.system('cls')
+
+# Variables name convention lowercase and snake_case
+MyVariable = "MyVariable invalid naming convention"
+print("\n")
+print(MyVariable)
+
+my_string_variable = "my_string_variable valid convention"
+print("\n")
+print(my_string_variable)
+
+my_int_variable = 5
+print("\n")
+print(my_int_variable)
+
+my_bool_varaible = True
+print("\n")
+print(my_bool_varaible)
+
+print("\n")
+print(my_bool_varaible, my_int_variable, my_string_variable)
+
+print("\n")
+print(my_bool_varaible, my_int_variable, my_string_variable, str(1.67))
+
+my_temp_var = str(5) * 6
+print("\n")
+print(my_temp_var)
