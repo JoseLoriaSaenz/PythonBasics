e4d21ac70eb9c966312f6f65aa53ae5881aa5a82
JoseLoriaSaenz <loriafjose@gmail.com>
2023-07-18 14:30:03 -0600

    Operators Commit

---
+++ 02_Operators.py
@@ -0,0 +1,51 @@
+import os
+os.system('cls')
+
+# Aritmethic Operators
+print('Aritmethic Operators')
+print(1 + 2)
+print(5 - 9)
+print(15 * 6)
+print(17 / 5)
+print(10 % 2) # Mod division
+print(10 // 4) # Floor Division
+print(2 ** 5)
+print(3 ** 5 + 2 - 1 / 6)
+
+# Concatenation symbol
+print("\n")
+print("Concatenation symbol")
+print("Hello + Python -->","Hello " + "Python")
+
+# String repetition using multiplication symbol
+print("\n")
+print("String Repetition using multiplication symbol ")
+print("Text * 2 * 3 -->","Text " * 2 * 3)
+
+my_var = 2 + 3
+print("Text * my_var having my_var equal to 5 -->","Text " * my_var)
+
+# Comparisson Operators
+print("\n")
+print("Comparisson Operators")
+print("Hello == Helo -->", "Hello" == "Helo")
+print("5*6 < 100 -->", 5*6 < 100)
+print("3 != 5 -->", 3 != 5)
+print("Hello > Helo -->", "Hello" > "Helo")
+print("Hello = Hello -->", "Hello" == "Hello")
+print("Hello = HELLO -->", "Hello" == "HELLO")
+
+# Logic Operators
+print("\n")
+print("Logic Operators")
+print("True and False -->", True and False)
+print("True or False -->", True or False)
+print("False and not False -->", False and not(False))
+
+x = 12
+x <<= 2
+print("x equals 12 <<= shifted left 2 -->", x)
+
+x = 48
+x >>= 2
+print("x equals 48 >>= shifted right 2 -->", x)
